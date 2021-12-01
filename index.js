79aedda2b42aefeb4897cba0e2388258b010f107
Prasanna Balaji D <83944299+prasanna0311@users.noreply.github.com>
2021-12-01 11:13:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import { userRouter } from "./routes/users.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+
+// mongo db connection
+
+const url =`mongodb+srv://prasanna:${process.env.MongoPassword}@cluster0.nx947.mongodb.net/myFirstDatabase`;
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
+const con = mongoose.connection;
+con.on("open", () => console.log("MongoDB is connected"));
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (request, respone) => {
+  respone.send("Welcome to Node app!!!! Hi Guys");
+});
+
+app.use("/users", userRouter);
+
+app.listen(PORT, () => console.log("The server is started in " + PORT));
+
+
